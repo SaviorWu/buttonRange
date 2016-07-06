7f7093518e6f345aa4eda0723593956e46e49cba
FriendWu <wuyoupeng@admindemac-mini-5.local>
2016-07-06 14:57:53 +0800

    Initial Commit

---
+++ buttonRange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  buttonRange
+//
+//  Created by 吴友鹏 on 16/7/6.
+//  Copyright © 2016年 FriendWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
